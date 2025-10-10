9f9295c3f2f72c2d95afc8a412781edd29246317
SriRajeswari-21 <srirajeswari.p@encora.com>
2025-10-10 15:59:06 +0530

    Add files via upload

---
+++ Day4(Oct-10th)/EmpEmployeeService/src/main/java/com/example/demo/EmpEmployeeServiceApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication(scanBasePackages = "com.example.controller")
+public class EmpEmployeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpEmployeeServiceApplication.class, args);
+	}
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+}
---
+++ Day4(Oct-10th)/EmpOrderService/src/main/java/com/example/controller/OrderController.java
@@ -0,0 +1,19 @@
+package com.example.controller;
+ 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.Order;
+
+import com.example.model.Order;
+ 
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+	@GetMapping("{id}")
+	public Order getOrderById(@PathVariable("id")int id) {
+		return new Order(id,"Laptop",20000.00);
+	}
+}
